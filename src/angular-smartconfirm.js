8c2fd5355b0580278661b61b59aef44d15597f7d
Alexander Hramov <alexander.hramov@gmail.com>
2016-12-27 10:51:45 +0300

    Add buttonClass param

--- src/angular-smartconfirm.js
+++ src/angular-smartconfirm.js
@@ -4,14 +4,15 @@ angular.module('angularSmartconfirm', [])
 		return{
 			restrict: 'EA',
 			scope:{
-				"confirm": "&"
+				"confirm": "&",
+				"buttonClass": "@"				
 			},
 			transclude: true,
 			template:
-			'<a ng-hide="show" class="btn btn-primary" ng-click="show=true"><ng-transclude/></a>' +
+			'<a ng-hide="show" class="btn btn-primary" ng-class="buttonClass" ng-click="show=true"><ng-transclude/></a>' +
 			'<div ng-show="show">' +
-			'<a href="#" class="btn btn-danger" ng-click="confirm()"><i class="fa fa-check fa-fw"></i></a> ' +
-			'<a href="#" class="btn btn-primary" ng-click="show=false"><i class="fa fa-times fa-fw"></i></a>' +
+			'<a href="#" class="btn btn-danger" ng-class="buttonClass" ng-click="confirm()"><i class="fa fa-check fa-fw"></i></a> ' +
+			'<a href="#" class="btn btn-primary" ng-class="buttonClass" ng-click="show=false"><i class="fa fa-times fa-fw"></i></a>' +
 			'</div>',
 			link: function(scope, element, attrs){
 			}
